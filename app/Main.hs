96b277ccd2e7b82f20ed7a81476455b8d8dc7d5d
Micah Cantor <micahcantor01@gmail.com>
2022-11-14 01:24:29 -0600

    new program runner

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
-import qualified MyGhci
+import qualified Synthesize.Run
+import System.Environment (getArgs)
 
 main :: IO ()
-main = MyGhci.startRepl
-
+main = do
+  args <- getArgs
+  case args of
+    [fileName, functionName] -> Synthesize.Run.runSynthesis fileName functionName
+    _ -> putStrLn "Usage: ghc-hacking <file name> <function name>"
---
+++ src/Synthesize/Run.hs
@@ -0,0 +1,44 @@
+module Synthesize.Run where
+
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import GHC (Ghc, HscEnv, runGhc)
+import qualified GHC
+import qualified GHC.Paths
+import Synthesize.MonadTransformer
+import qualified System.FilePath as FilePath
+
+-- Create a new GHC interactive session
+initSession :: IO HscEnv
+initSession = GHC.runGhc (Just GHC.Paths.libdir) $ do
+  dflags <- GHC.getSessionDynFlags
+  GHC.setSessionDynFlags (dflags {GHC.ghcLink = GHC.LinkInMemory, GHC.backend = GHC.Interpreter})
+  GHC.getSession
+
+-- Run a Ghc action with a given environment
+withEnv :: HscEnv -> Ghc () -> IO HscEnv
+withEnv env action = GHC.runGhc (Just GHC.Paths.libdir) $ do
+  GHC.setSession env
+  action
+  GHC.getSession
+
+-- Load a Haskell file into an interactive session
+load :: String -> Ghc ()
+load path = do
+  target <- GHC.guessTarget path Nothing
+  GHC.addTarget target
+  loadResult <- GHC.load GHC.LoadAllTargets
+  case loadResult of
+    GHC.Succeeded -> do
+      let loadedModule = GHC.IIModule (GHC.mkModuleName (FilePath.takeBaseName path))
+      ctx <- GHC.getContext
+      GHC.setContext (loadedModule : ctx)
+      liftIO $ putStrLn ("Successfully loaded file: " <> path)
+    GHC.Failed ->
+      liftIO $ putStrLn ("Failed to load file: " <> path)
+
+runSynthesis :: FilePath -> String -> IO ()
+runSynthesis fileName functionName = do
+  initialEnv <- initSession
+  loadedEnv <- withEnv initialEnv (load fileName)
+  synthesisResult <- makeRunStack loadedEnv functionName
+  print synthesisResult
